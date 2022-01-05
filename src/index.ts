e9ed72e5ea3f5c3dae3acfc3263b8ce1ebb74ea9
KotoVoyou <paillette.benjamin@gmail.com>
2022-01-05 09:34:32 +0100

    Ajout de fichier statique

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,16 @@ import express from "express";
 
 const app = express();
 
-app.get("/", function (req, res) {
-    res.send("Hello World!");
-});
-
-app.get("/scalingo", (req, res) => {
+app.get("/hello", (req, res) => {
     res.send("Hello scalingo!");
 });
+
+// const staticDirectory = "/" + (process.env.STATIC_DIR || "dist");
+const staticDirectory = `${__dirname}/${process.env.STATIC_DIR || "dist"}`;
+app.use(express.static(staticDirectory));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
+    console.log(`Serving static directory ${staticDirectory}`);
 });
