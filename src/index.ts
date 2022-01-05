d3337d688a71556c4491d5b0715bde840cf257c1
KotoVoyou <paillette.benjamin@gmail.com>
2022-01-05 09:11:58 +0100

    Lecture du port dans env

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ app.get("scalingo", (req, res) => {
     res.send("Hello scalingo!");
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
 });
