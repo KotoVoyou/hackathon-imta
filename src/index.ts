61c53381be1cbefa8e0316868ed839e50fecd888
KotoVoyou <paillette.benjamin@gmail.com>
2022-01-05 09:01:42 +0100

    Changement build scalingo

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,11 @@ import express from "express";
 const app = express();
 
 app.get("/", function (req, res) {
-    res.send("Hello Scalingo!");
+    res.send("Hello World!");
+});
+
+app.get("scalingo", (req, res) => {
+    res.send("Hello scalingo!");
 });
 
 const port = 3000;
