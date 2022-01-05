03128de5aec30ababb9c4a56fb8f7038bfc45ce6
KotoVoyou <paillette.benjamin@gmail.com>
2022-01-05 09:14:46 +0100

    Erreur url

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ app.get("/", function (req, res) {
     res.send("Hello World!");
 });
 
-app.get("scalingo", (req, res) => {
+app.get("/scalingo", (req, res) => {
     res.send("Hello scalingo!");
 });
 
