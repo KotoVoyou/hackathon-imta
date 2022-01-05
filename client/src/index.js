07a59ce4a06add95bff9226e56ec79336b1e96fd
Paul Beslin <paul.beslin@gmail.com>
2022-01-05 11:49:01 +0100

    Client pour la multiplication par deux d'un nombre (ne fonctionne pas)

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,74 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+/*
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  document.getElementById('root')
+);*/
+
+/*var element = React.createElement('h1', { className: 'greeting' }, 'Hello, world!');
+ReactDOM.render(element, document.getElementById('root'));*/
+
+class NumberForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: '', result: ''};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {    this.setState({value: event.target.value});  }
+  handleSubmit(event) {
+    var form = new FormData();
+    form.append("number", this.state.value);
+
+    fetch("https://hackathon-imta.osc-fr1.scalingo.io/api/multiply2", {
+      method: "PUT",
+      body: form
+    })
+      .then(res => res.json())
+      .then(
+        (res) => {
+          console.log("HI");
+          this.setState((state, props) => (
+              {result: res.result}
+            )
+          )
+        }
+      );
+
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Nombre à doubler :
+            <input type="number" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Envoyer" />
+        </form>
+        <p>Résultat : {this.state.result}</p>
+      </div>
+    );
+  }
+}
+
+function App(){
+  return (
+    <NumberForm/>
+  );
+}
+
+ReactDOM.render(
+  <App/>,
   document.getElementById('root')
 );
 
