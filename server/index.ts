3c81cab3dc479f8ae8c8f563b2a06377138e36ce
KotoVoyou <paillette.benjamin@gmail.com>
2022-01-07 11:22:43 +0100

    Ajout d'une route pour diviser par 2

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 
+import axios from "axios";
+
 const app = express();
 
 app.use(cors());
@@ -16,6 +18,30 @@ app.put("/api/multiply2", (req, res) => {
     res.status(200).json({
         result: x * 2,
     });
+});
+
+interface MathJsAPIResponse {
+    result: string;
+    error: string | null;
+}
+
+app.put("/api/divide2", (req, res) => {
+    if (!req.body.number) return res.status(400).send("Number is missing");
+
+    let x: number = req.body.number;
+
+    axios
+        .post("http://api.mathjs.org/v4/", {
+            expr: `${x}/2`,
+        })
+        .then((response) => response.data as MathJsAPIResponse)
+        .then((data) => {
+            if (data.error) return res.status(400).send(data.error);
+            return res.status(200).json({
+                result: parseFloat(data.result),
+            });
+        })
+        .catch((_) => res.status(500).send("An error occurs"));
 });
 
 const staticDirectory = `${__dirname}/${process.env.STATIC_DIR || "dist"}`;
