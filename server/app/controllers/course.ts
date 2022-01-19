21f2f46ef3f8df35c050df6c431ec53a3acaa1c9
KotoVoyou <paillette.benjamin@gmail.com>
2022-01-19 11:01:07 +0100

    Creation, modificiation et suppression d'un cours

--- server/app/controllers/course.ts
+++ server/app/controllers/course.ts
@@ -3,6 +3,8 @@ import {
     courseModel as model,
     Course,
     CourseFilter,
+    CourseInput,
+    CourseUpdate,
 } from "../../database/models/course";
 
 export const getCourses = (filter: CourseFilter = {}): Promise<Array<Course>> =>
@@ -18,5 +20,32 @@ export const getCourseById = (id: ObjectId): Promise<Course> =>
         model
             .findById(id)
             .then((course) => resolve(course))
+            .catch((error) => reject(error));
+    });
+
+export const createCourse = (newCourse: CourseInput): Promise<Course> =>
+    new Promise((resolve, reject) => {
+        model
+            .create(newCourse)
+            .then((course) => resolve(course))
+            .catch((error) => reject(error));
+    });
+
+export const updateCourse = (
+    id: ObjectId,
+    update: CourseUpdate
+): Promise<Course> =>
+    new Promise((resolve, reject) => {
+        model
+            .findOneAndUpdate({ _id: id }, update, { new: true })
+            .then((course) => resolve(course))
+            .catch((error) => reject(error));
+    });
+
+export const deleteCourse = (id: ObjectId): Promise<boolean> =>
+    new Promise((resolve, reject) => {
+        model
+            .deleteOne({ _id: id })
+            .then((_) => resolve(true))
             .catch((error) => reject(error));
     });
