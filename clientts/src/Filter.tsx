fc45c9d17b5dc1a143958c3920cd795d56a787fb
Paul Beslin <paul.beslin@gmail.com>
2022-01-19 11:28:06 +0100

    Filters are now working

--- clientts/src/Filter.tsx
+++ clientts/src/Filter.tsx
@@ -1,46 +1,89 @@
+import React from "react";
 import { ReactElement } from "react";
+import { Campus, Slots, TAFs } from "./enums";
 import './Filter.css'
+import { FilterBlockConfig } from "./UETable";
 
 interface Filter{
     category: string,
     options: string[]
 }
 
-function FilterBlock(props: Filter): ReactElement{
-    const rows: React.ReactNode[] = props.options.map((option: string, index: number) => {
+type FilterBlockProps = Filter & {onSelectionChange: (e: string[]) => void}
+
+function removeItem<T>(arr: Array<T>, value: T): Array<T> { 
+    const index = arr.indexOf(value);
+    if (index > -1) {
+      arr.splice(index, 1);
+    }
+    return arr;
+}
+
+class FilterBlock extends React.Component<FilterBlockConfig, {selection: string[]}>{
+    constructor(props: FilterBlockConfig){
+        super(props);
+        this.state = {selection: props.selected};
+        this.handleSelectionChange = this.handleSelectionChange.bind(this);
+    }
+
+    handleSelectionChange(e: React.FormEvent<HTMLInputElement>, optionName: string){
+        if(e.currentTarget.checked && !this.props.selected.includes(optionName)){
+            this.state.selection.push(optionName);
+            //Refresh
+            this.render();
+        }
+        if(!e.currentTarget.checked && this.props.selected.includes(optionName)){
+            this.setState((state, props) => {
+                selection: removeItem(state.selection, optionName)
+            })
+        }
+        this.props.handler(this.state.selection);
+    }
+
+    render(){
+        const rows: React.ReactNode[] = this.props.options.map((option: string, index: number) => {
+            return (
+                <p>
+                    <input type="checkbox" name={index.toString()} checked={this.state.selection.includes(option)} 
+                    onClick={(e: React.FormEvent<HTMLInputElement>) => this.handleSelectionChange(e, option)}/>
+                    <label htmlFor={index.toString()}>{option}</label>
+                </p>
+            )
+        });
+    
+        return(
+            <div className="category">
+                <h4>{this.props.categoryName}</h4>
+                <div className="categoryOptions">{rows}</div>
+            </div>
+        );
+    }
+}
+
+export interface FilterOptions{
+    slotOptions: string[],
+    campusOptions: string[],
+    tafOptions: string[]
+}
+
+class FilterList extends React.Component<{blocks: FilterBlockConfig[]}, {}>{
+    constructor(props: {blocks: FilterBlockConfig[]}){
+        super(props);
+    }
+
+    render(): React.ReactNode {
+        const rows: React.ReactNode[] = [];
+        this.props.blocks.forEach((block: FilterBlockConfig) => {
+            rows.push(<FilterBlock categoryName={block.categoryName} options={block.options} selected={block.selected} handler={block.handler}/>)
+        });
+
         return (
-            <p>
-                <input type="checkbox" name={index.toString()}/>
-                <label htmlFor={index.toString()}>{option}</label>
-            </p>
-        )
-    });
+            <div>
+                {rows}
+            </div>
+        );
+    }
 
-    return(
-        <div className="category">
-            <h4>{props.category}</h4>
-            <div className="categoryOptions">{rows}</div>
-        </div>
-    );
 }
 
-const filters: Filter[] = [
-    {category: "Créneau", options: ['D', 'E', 'F']},
-    {category: "Campus", options: ['Brest', 'Nantes', 'Rennes']},
-    {category: "TAF", options: ['LOGIN', 'COPSI']}
-]
-
-function App2(): ReactElement{
-    const blocks: React.ReactNode[] = [];
-    filters.forEach((filter) => {
-        blocks.push(<FilterBlock category={filter.category} options={filter.options}/>)
-    })
-
-    return (
-        <div>
-            {blocks}
-        </div>
-    );
-}
-
-export default App2;
+export default FilterList;
